1917910c758e116526406a78c7bdbeab95499f8c
puiminh <volmicrolab@gmail.com>
2024-03-17 22:38:05 +0700

    Homepage

--- src/components/barComponents/LogoDefault.vue
+++ src/components/barComponents/LogoDefault.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <router-link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="/images/recipe-favicon-black.png" class="h-14" alt="Flowbite Logo" />
-            <span class="self-center text-3xl font-semibold whitespace-nowrap dm-serif orange">Recipes.</span>
+            <img src="/images/recipe-favicon-black.png" class="h-10" alt="Flowbite Logo" />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dm-serif orange">Recipes.</span>
         </router-link>
     </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,35 @@
 <template>
   <div class="flex w-full h-full">
-    <div class="left-bar bg-white w-1/5 pt-8 flex flex-col">
+    <div class="left-bar bg-white w-1/6 pt-8 flex flex-col">
       <LogoDefault class="mb-10 mx-auto"></LogoDefault>
 
       <div class="flex flex-col gap-4 pl-12">
         <div class="menu-item flex items-center gap-4 relative">
-          <IconDashboard :checked="true" class="h-8"></IconDashboard>
-          <h2 class=" font-bold text-slate-800 text-lg">Explore</h2>
+          <IconDashboard :checked="true" class="h-6"></IconDashboard>
+          <h2 class="  font-bold text-slate-800 ">Home</h2>
           <div class="w-1 h-full absolute right-0 bg-black"></div>
         </div>
         <div class="menu-item flex items-center gap-4 relative">
-          <IconHeart :checked="false" class="h-8"></IconHeart>
-          <h2 class="font-semibold text-slate-600 text-lg">Favorites</h2>
+          <IconExplore :checked="false" class="h-6"></IconExplore>
+          <h2 class="  font-bold text-slate-600 ">Explore</h2>
         </div>
         <div class="menu-item flex items-center gap-4 relative">
-          <IconMap :checked="false" class="h-8"></IconMap>
-          <h2 class="font-semibold text-slate-600 text-lg">Store</h2>
+          <IconHeart :checked="false" class="h-6"></IconHeart>
+          <h2 class=" font-bold text-slate-600 ">Favorites</h2>
         </div>
         <div class="menu-item flex items-center gap-4 relative">
-          <IconSetting :checked="false" class="h-8"></IconSetting>
-          <h2 class="font-semibold text-slate-600 text-lg">Setting</h2>
+          <IconMap :checked="false" class="h-6"></IconMap>
+          <h2 class=" font-bold text-slate-600 ">Store</h2>
+        </div>
+        <div class="menu-item flex items-center gap-4 relative">
+          <IconSetting :checked="false" class="h-6"></IconSetting>
+          <h2 class=" font-bold text-slate-600 ">Setting</h2>
         </div>
       </div>
 
-      <div class="relative flex flex-col items-center mb-4 h-full">
-        <img class="z-10 absolute -top-2 h-52 left-12" src="/images/cooking1.webp" alt="">
-        <div class="absolute flex flex-col bottom-0 pt-12 pb-6 px-8 rounded-2xl bg-orange font-bold text-white text-lg">
+      <div class="flex flex-col items-center mb-4 mt-auto relative">
+          <img class="z-10 absolute -top-36 left-8 h-48" src="/images/cooking1.webp" alt="">
+        <div class="flex flex-col bottom-0 pt-12 pb-6 m-4 px-8 rounded-2xl bg-orange font-bold text-white text-md">
           <p>Share your <span class="font-extrabold">Recipes</span> <br></p> 
           <p>with the whole world</p> 
 
@@ -36,9 +40,14 @@
 
     </div>
     <div class="content w-4/5 flex">
-      <div class="middle-content bg-lightGray w-3/4">
+      <div class="bg-lightGray w-3/4">
+          <div class="bg-white rounded-3xl flex gap-4 mx-16 px-4 items-center mt-8">
+            <IconSearch class="h-6"></IconSearch>
+            <input type="text" class="bg-white p-2 font-bold w-full" placeholder="Search for Recipes, Ingredients and Tags">
+          </div>
+
       </div>
-      <div class="right-bar bg-white w-1/4">
+      <div class="bg-white w-1/4">
       </div>
     </div>
 
@@ -51,6 +60,8 @@ import IconHeart from '@/components/icons/IconHeart.vue';
 import IconProfile from '@/components/icons/IconProfile.vue';
 import IconSetting from '@/components/icons/IconSetting.vue';
 import IconMap from '@/components/icons/IconMap.vue';
+import IconSearch from '@/components/icons/IconSearch.vue';
+import IconExplore from '@/components/icons/IconExplore.vue';
 
 
 
@@ -63,6 +74,8 @@ export default {
     IconProfile,
     IconSetting,
     IconMap,
+    IconSearch,
+    IconExplore
   }
 
   
