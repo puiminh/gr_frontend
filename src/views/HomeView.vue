80774184067ec5752ca51918f02313c385cf9c4d
puiminh <volmicrolab@gmail.com>
2024-03-18 21:58:20 +0700

    middle content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -30,13 +30,13 @@
       </div>
 
       <div class="flex flex-col items-center mb-4 mt-auto relative">
-          <img style="transform: translate(-50%, 0);" class="z-10 absolute -top-36 upload-box left-1/2" src="/images/cooking3.webp" alt="">
+          <img style="transform: translate(-50%, -70%);" class="z-10 absolute upload-box left-1/2" src="/images/cooking3.webp" alt="">
         <div class="flex flex-col bottom-0 pt-12 pb-6 m-4 px-8 rounded-2xl bg-orange font-bold text-white text-md upload-box">
           <p>Share your <span class="font-extrabold">Recipes</span> <br></p> 
           <p>with the whole world</p> 
 
           <div class="flex items-center mt-4">
-            <button class="orange bg-white font-bold px-3 py-2 rounded-xl mx-auto">Go to Workspace</button>
+            <button class="orange bg-white font-bold px-3 py-2 rounded-xl mx-auto">Workspace</button>
             <button
                     type="button" class="h-min bg-gray-800 rounded-full ring-4 ring-white">
                     <img class="w-8 h-8 rounded-full object-cover" :src="user.avatar" alt="user photo">
@@ -48,8 +48,8 @@
 
 
     </div>
-    <div class="content w-4/5 flex">
-      <div class="bg-lightGray w-3/4 px-8">
+    <div class="content w-4/5 flex h-screen overflow-auto">
+      <div class="bg-lightGray w-3/4 px-8 h-screen overflow-y-auto overflow-x-hidden">
           
           <!-- Search Bar -->
           <div class="bg-white rounded-3xl flex gap-4 mx-16 px-4 items-center mt-8">
@@ -57,6 +57,7 @@
             <input type="text" class="bg-white p-2 font-bold w-full" placeholder="Search for Recipes, Ingredients and Tags">
           </div>
 
+          <!-- Banner -->
           <div class="w-full  flex h-40  mt-10 relative ">
             <div class="bg-orange custom-shape w-full h-full rounded-2xl  text-white pt-10 pb-8 px-8">
               <h1 class="font-extrabold text-2xl">
@@ -71,11 +72,60 @@
             <img class="absolute h-48 right-10 -top-10 z-10" src="/images/model.png" alt="">
           </div>
 
+          <!-- Categories -->
+
+          <section class="mt-6 ">
+            <h1 class="font-extrabold mb-6 text-xl">Categories</h1>
+            <div class="grid grid-cols-4 gap-4 ">
+              
+              <div class="flex items-center justify-center relative">
+                <img class="w-full h-16 brightness-50 object-cover rounded-xl" src="/images/breakfast.jpg" alt="">
+                <h1 class="font-bold text-white text-xl absolute">Breakfast</h1>
+              </div>
+
+              <div class="flex items-center justify-center relative">
+                <img class="w-full h-16 brightness-50 object-cover rounded-xl" src="/images/lunch.jpg" alt="">
+                <h1 class="font-bold text-white text-xl absolute">Lunch</h1>
+              </div>
+
+              <div class="flex items-center justify-center relative">
+                <img class="w-full h-16 brightness-50 object-cover rounded-xl" src="/images/dinner.jpg" alt="">
+                <h1 class="font-bold text-white text-xl absolute">Dinner</h1>
+              </div>
+
+              <div class="flex items-center justify-center relative">
+                <img class="w-full h-16 brightness-50 object-cover rounded-xl" src="/images/dessert.jpg" alt="">
+                <h1 class="font-bold text-white text-xl absolute">Dessert</h1>
+              </div>
+            </div>
+          </section>
+
+          <!-- Popular -->
+
+          <section class="mt-6">
+            <h1 class="font-extrabold mb-6 text-xl">Popular Recipes</h1>
+
+            <div class="grid grid-cols-3 gap-4">
+              <div 
+                  v-for="recipe in recipes" :key="recipe.id">
+                  <RouterLink :to="'/recipe/'+recipe.id">
+                      <div class="flex justify-center flex-col h-96">
+                          <img class="h-full w-full object-cover rounded-xl" 
+                              :src="recipe.image" alt="">
+                      </div>
+                      <div class="">
+                          <p class="text-xl font-semibold my-3">{{recipe.name}}</p>
+                      </div>
+                  </RouterLink>
+      
+                </div>
+            </div>
+          </section>
 
 
 
       </div>
-      <div class="bg-white w-1/4">
+      <div class="bg-white w-1/4 h-screen">
         <div class="grid-cols-2">
           <div class="">
 
@@ -98,6 +148,8 @@ import IconExplore from '@/components/icons/IconExplore.vue';
 import { mapState, mapActions } from 'pinia';
 import { useAuthStore } from '@/stores/auth';
 import IconLogout from '@/components/icons/IconLogout.vue';
+import data from '../db.json'
+import StarsRatingDisplay from '@/components/stars/StarsRatingDisplay.vue';
 
 
 
@@ -106,6 +158,8 @@ export default {
   data () {
     return {
       openUserDropdown: false,
+      recipes: data.recipes,
+
       user: {
         first_name: "Minh",
         last_name: "Bui",
@@ -153,6 +207,7 @@ export default {
     IconSearch,
     IconExplore,
     IconLogout,
+    StarsRatingDisplay,
   },
   mounted () {
     if (this.isAuthenticated) {
@@ -172,7 +227,7 @@ export default {
 }
 
 .upload-box {
-  width: calc(100% - 3rem);
+  width: calc(100% - 2rem);
 }
 
 </style>
