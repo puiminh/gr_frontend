2682d7766aa42dbbe2891a005403ffe90d826f7b
puiminh <volmicrolab@gmail.com>
2024-03-24 21:06:57 +0700

    add logo

--- src/components/barComponents/LogoDefault.vue
+++ src/components/barComponents/LogoDefault.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <router-link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="/images/recipe-favicon-black.png" class="h-12" alt="Flowbite Logo" />
+            <img src="/images/logo.png" class="h-12" alt="Flowbite Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dm-serif orange">Recipes.</span>
         </router-link>
     </div>
